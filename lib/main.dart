ed764a31e29ab5f537c40aeb6122bc16d44d0f30
Kylin <unikylin@126.com>
2021-02-09 16:16:17 +0800

    fix: main

--- lib/main.dart
+++ lib/main.dart
@@ -8,23 +8,17 @@ import 'package:jh_flutter_demo/project/new_feature/new_feature_page.dart';
 import 'package:package_info/package_info.dart';
 
 import 'package:flustars/flustars.dart';
-import 'package:flui/src/widgets/toast.dart';
 import 'package:oktoast/oktoast.dart';
 import 'package:fluro/fluro.dart';
-
-import 'package:jhtoast/jhtoast.dart';
 
 import 'project/configs/colors.dart';
 import 'project/routes/routes_old.dart' as luyou;
 import 'project/routes/routes.dart';
 import 'project/routes/application.dart';
-import 'project/home_page.dart';
 import 'project/base_tabbar.dart';
 import 'project/login/pages/login_page.dart';
 import 'project/model/user_model.dart';
 import 'project/configs/project_config.dart';
-
-import 'jh_common/utils/jh_screen_utils.dart';
 import 'jh_common/utils/jh_storage_utils.dart';
 import 'jh_common/widgets/jh_alert.dart';
 
@@ -46,8 +40,6 @@ import 'jh_common/widgets/jh_alert.dart';
     bottomBarHeight = mq.padding.bottom;
 
  * */
-
-//void main() => runApp(MyApp());
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -80,12 +72,10 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  FLToastDefaults _toastDefaults = FLToastDefaults();
   var _currentVersion = '';
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
 
     LogUtils.init();
@@ -97,74 +87,30 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-//    JhScreenUtils.init(context);
-    return
-//      FLToastProvider(
-//          defaults: _toastDefaults,
-        OKToast(
-//          dismissOtherOnShow: true,
-            child: Container(
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-//                brightness: //深色还是浅色
-//                primarySwatch: Colors.blue //主题颜色样本
-          primaryColor: KColor.kWeiXinThemeColor, //主色，决定导航栏颜色
-          accentColor: KColor.kWeiXinTitleColor,
-          //次级色，决定大多数Widget的颜色，如进度条、开关等。
-          primaryIconTheme: IconThemeData(color: KColor.kWeiXinTitleColor),
+    return OKToast(
+      child: Container(
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primaryColor: KColor.kWeiXinThemeColor, //主色，决定导航栏颜色
+            accentColor: KColor.kWeiXinTitleColor,
+            primaryIconTheme: IconThemeData(color: KColor.kWeiXinTitleColor),
+          ),
+          home: switchRootWidget(),
+          onGenerateRoute: Application.router.generator,
+          onUnknownRoute: (RouteSettings settings) =>
+              MaterialPageRoute(builder: (context) => luyou.UnknownPage()),
+          localizationsDelegates: [
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            const FallbackCupertinoLocalisationsDelegate()
+          ],
+          supportedLocales: [
+            Locale('zh', 'CN'),
+          ],
         ),
-//            home: IndexPage(),
-//            home: BaseTabBar(),
-        home: SwitchRootWidget(),
-        // 注册路由
-//              routes: luyou.routes,
-        onGenerateRoute: Application.router.generator,
-        onUnknownRoute: (RouteSettings settings) =>
-            MaterialPageRoute(builder: (context) => luyou.UnknownPage()),
-        //        locale: Locale('en','US'),
-        localizationsDelegates: [
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          const FallbackCupertinoLocalisationsDelegate()
-        ],
-        supportedLocales: [
-          Locale('zh', 'CN'),
-//                Locale('en', 'US')
-        ],
       ),
-    ));
-
-// -------------------------- 以下不使用oktoast------------------------------------
-//      Container(
-//      child: MaterialApp(
-//        debugShowCheckedModeBanner: false,
-//        theme: ThemeData(
-////          brightness: //深色还是浅色
-////            primarySwatch: Colors.blue //主题颜色样本
-//            primaryColor: KColor.kWeiXinThemeColor,  //主色，决定导航栏颜色
-//            accentColor:  KColor.kWeiXinTitleColor, //次级色，决定大多数Widget的颜色，如进度条、开关等。
-//            primaryIconTheme: IconThemeData(color: KColor.kWeiXinTitleColor),
-//
-//        ),
-//        home: IndexPage(),
-//        // 注册路由
-//        routes: luyou.routes,
-//        onUnknownRoute: (RouteSettings settings) =>
-//            MaterialPageRoute(builder: (context) => luyou.UnknownPage()),
-//        //        locale: Locale('en','US'),
-//        localizationsDelegates: [
-//          GlobalMaterialLocalizations.delegate,
-//          GlobalWidgetsLocalizations.delegate,
-//          GlobalEasyRefreshLocalizations.delegate
-//        ],
-//        supportedLocales: [
-////          Locale('en','US'),
-//          Locale('zh','CN'),
-//        ],
-//      ),
-//    );
-// -------------------------- 以下不使用oktoast------------------------------------
+    );
   }
 
   void _getInfo() async {
@@ -179,22 +125,14 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
-  Widget SwitchRootWidget() {
+  Widget switchRootWidget() {
     var lastVersion = JhStorageUtils.getStringWithKey(kUserDefault_LastVersion);
-//    print('lastVersion 版本号：$lastVersion');
     if (lastVersion == null || lastVersion == '') {
-//      print('首次安装');
       return NewFeaturePage();
     } else {
-//      print(oldVersion.compareTo(_currentVersion)); // 字符串 比较大小, 0:相同、1:大于、-1:小于
       if (lastVersion.compareTo(_currentVersion) < 0) {
-//        print('新版本安装');
         return NewFeaturePage();
       } else {
-//        print('正常启动');
-//        userModel model =
-//            SpUtil.getObj(kUserDefault_UserInfo, (v) => userModel.fromJson(v));
-
         var modelJson = JhStorageUtils.getModelWithKey(kUserDefault_UserInfo);
         if (modelJson != null) {
           UserModel model = UserModel.fromJson(modelJson);
@@ -207,21 +145,3 @@ class _MyAppState extends State<MyApp> {
     }
   }
 }
-
-//void main() => runApp(MyApp());
-//class MyApp extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return new MaterialApp(
-//      title: 'Welcome to Flutter',
-//      home: new Scaffold(
-//        appBar: new AppBar(
-//          title: new Text('Welcome to Flutter'),
-//        ),
-//        body: new Center(
-//          child: new Text('Hello World'),
-//        ),
-//      ),
-//    );
-//  }
-//}
